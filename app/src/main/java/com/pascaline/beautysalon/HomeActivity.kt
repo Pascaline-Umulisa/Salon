9fb9032afef8af10c1cad459571fbc5f9e939db1
Pascaline-Umulisa <kayiteteumulisa@gmail.com>
2022-07-10 16:22:08 +0300

    added splash screen and fragments

---
+++ app/src/main/java/com/pascaline/beautysalon/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.pascaline.beautysalon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/pascaline/beautysalon/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.pascaline.beautysalon
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this,HomeActivity::class.java))
+        finish()
+    }
+}

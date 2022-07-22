0a7292169227ab29420c91f94199ba1572db60e6
Pascaline-Umulisa <kayiteteumulisa@gmail.com>
2022-07-22 11:19:17 +0300

    added fragments

--- app/src/main/java/com/pascaline/beautysalon/HomeActivity.kt
+++ app/src/main/java/com/pascaline/beautysalon/HomeActivity.kt
@@ -2,10 +2,41 @@ package com.pascaline.beautysalon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pascaline.beautysalon.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var binding:ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding=ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupBottomNav()
     }
+    fun setupBottomNav() {
+        binding.bnvHome.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    val transaction = supportFragmentManager.beginTransaction().replace(R.id.fcvHome, HomeFragment()).commit()
+                    true }
+
+                R.id.product -> {
+                    val transaction = supportFragmentManager.beginTransaction().replace(R.id.fcvHome, ProductFragment()).commit()
+                    true }
+
+                R.id.service -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fcvHome, ServiceFragment()).commit()
+                    true }
+
+                R.id.book -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fcvHome, BookFragment()).commit()
+                    true }
+
+                else -> false
+
+
+            }
+        }
+    }
+
+
 }
